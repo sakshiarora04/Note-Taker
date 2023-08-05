8b7a528e3eb837ac1d0cbfe129dc6030272e5c09
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-05 23:38:28 +1000

    notes post

--- routes/notes.js
+++ routes/notes.js
@@ -8,5 +8,30 @@ notes.get('/', (req, res) =>
 );
 
 // POST Route for submitting notes
+notes.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { noteTitle, noteText } = req.body;
+
+  // If all the required properties are present
+  if (noteTitle && noteText) {
+    // Variable for the object we will save
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newFeedback, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in adding note');
+  }
+});
 
 module.exports = notes;
